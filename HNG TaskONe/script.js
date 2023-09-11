c14575fc0a97e3d0c698d44d9ee005ffc4f6c389
Ahamba Francis Ugochukwu <143269108+Dev-lex97@users.noreply.github.com>
2023-09-11 11:39:15 +0100

    Update script.js

--- HNG TaskONe/script.js
+++ HNG TaskONe/script.js
@@ -7,3 +7,18 @@ document.querySelector('[data-testid="currentUTCTime"]').textContent = currentDa
 
 setInterval(updateDateTime, 1000);
 
+// Function to display the day of the week
+const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+  const day = new Date().getDay();
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = daysOfTheWeek[day];
+
+//function expression to update time
